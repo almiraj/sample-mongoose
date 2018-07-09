582bf64eb78c05f376fb5753d93fd15a3c358181
almiraj <rain@almiraj.net>
2018-07-09 20:55:27 +0900

    Add create sample

--- README.md
+++ README.md
@@ -1,76 +1,128 @@
 # sample-mongoose
 
 ```js
+const AModel = mongoose.model('AModel', new Schema({
+  col1: { type: Number }, col2: { type: String }
+}));
 const o1 = { col1: 3, col2: 'a3' };
 const o2 = { col1: 3, col2: 'aaa333' };
 
 // * upsert *
 
 // reset
-Aaa.remove({})
-// -> { n: 2, ok: 1 }
+AModel.remove({})
+// -> { n: 3, ok: 1 }
 
-// check content
-Aaa.findOne({ col1: o1.col1 })
-// -> null
+// find all
+AModel.find({})
+// -> []
 
 // update not found
-Aaa.update({ col1: o1.col1 }, o1)
+AModel.update({ col1: o1.col1 }, o1)
 // -> { n: 0, nModified: 0, ok: 1 }
 
 // insert by upsert
-Aaa.update({ col1: o1.col1 }, o1, { upsert: true })
+AModel.update({ col1: o1.col1 }, o1, { upsert: true })
 // -> { n: 1,
 //      nModified: 0,
-//      upserted: [ { index: 0, _id: 5b433b933a9b509714b9982e } ],
+//      upserted: [ { index: 0, _id: 5b434c6b3a9b509714b99d51 } ],
 //      ok: 1 }
 
-// check content
-Aaa.findOne({ col1: o1.col1 })
-// -> { _id: 5b433b933a9b509714b9982e, col1: 3, __v: 0, col2: 'a3' }
+// find all
+AModel.find({})
+// -> [ { _id: 5b434c6b3a9b509714b99d51, col1: 3, __v: 0, col2: 'a3' } ]
 
 // update
-Aaa.update({ col1: o1.col1 }, o1)
+AModel.update({ col1: o1.col1 }, o1)
 // -> { n: 1, nModified: 0, ok: 1 }
 
 // update the same object
-Aaa.update({ col1: o1.col1 }, o1, { upsert: true })
+AModel.update({ col1: o1.col1 }, o1, { upsert: true })
 // -> { n: 1, nModified: 0, ok: 1 }
 
 // update the different object
-Aaa.update({ col1: o2.col1 }, o2)
+AModel.update({ col1: o2.col1 }, o2)
 // -> { n: 1, nModified: 1, ok: 1 }
+
+// find all
+AModel.find({})
+// -> [ { _id: 5b434c6b3a9b509714b99d51, col1: 3, __v: 0, col2: 'aaa333' } ]
 
 // * save *
 
 // reset
-Aaa.remove({})
+AModel.remove({})
 // -> { n: 1, ok: 1 }
 
-// check content
-Aaa.findOne({ col1: o1.col1 })
-// -> null
+// find all
+AModel.find({})
+// -> []
 
 // save
-new Aaa(o1).save()
-// -> { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'a3', __v: 0 }
+new AModel(o1).save()
+// -> { _id: 5b434c6bdc5d9f1ef42d9163, col1: 3, col2: 'a3', __v: 0 }
 
-// check content
-Aaa.findOne({ col1: o1.col1 })
-// -> { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'a3', __v: 0 }
+// save the same object
+new AModel(o1).save()
+// -> { _id: 5b434c6bdc5d9f1ef42d9164, col1: 3, col2: 'a3', __v: 0 }
 
 // save the different object
-new Aaa(o2).save()
-// -> { _id: 5b433b9333ef93257cc52e79, col1: 3, col2: 'aaa333', __v: 0 }
+new AModel(o2).save()
+// -> { _id: 5b434c6bdc5d9f1ef42d9165, col1: 3, col2: 'aaa333', __v: 0 }
 
-// check content
-Aaa.findOne({ col1: o2.col1 })
-// -> { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'a3', __v: 0 }
+// find all
+AModel.find({})
+// -> [ { _id: 5b434c6bdc5d9f1ef42d9163, col1: 3, col2: 'a3', __v: 0 },
+//      { _id: 5b434c6bdc5d9f1ef42d9164, col1: 3, col2: 'a3', __v: 0 },
+//      { _id: 5b434c6bdc5d9f1ef42d9165, col1: 3, col2: 'aaa333', __v: 0 } ]
 
-// find and save
-Aaa.findOne({ col1: o2.col1 }).then(o => { o.col2 = 'SAVED'; return o.save(); })
-// -> { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'SAVED', __v: 0 }
+// findOne and save
+AModel.findOne({ col1: o2.col1 }).then(o => { o.col2 = 'AAA333'; return o.save(); })
+// -> { _id: 5b434c6bdc5d9f1ef42d9163, col1: 3, col2: 'AAA333', __v: 0 }
 
-// check content
-Aaa.findOne({ col1: o2.col1 })
-// -> { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'SAVED', __v: 0 }
+// find all
+AModel.find({})
+// -> [ { _id: 5b434c6bdc5d9f1ef42d9163, col1: 3, col2: 'AAA333', __v: 0 },
+//      { _id: 5b434c6bdc5d9f1ef42d9164, col1: 3, col2: 'a3', __v: 0 },
+//      { _id: 5b434c6bdc5d9f1ef42d9165, col1: 3, col2: 'aaa333', __v: 0 } ]
+
+// * create *
+
+// reset
+AModel.remove({})
+// -> { n: 3, ok: 1 }
+
+// find all
+AModel.find({})
+// -> []
+
+// create
+AModel.create(o1)
+// -> { _id: 5b434c6bdc5d9f1ef42d9166, col1: 3, col2: 'a3', __v: 0 }
+
+// create the same object
+AModel.create(o1)
+// -> { _id: 5b434c6bdc5d9f1ef42d9167, col1: 3, col2: 'a3', __v: 0 }
+
+// create the different object
+AModel.create(o2)
+// -> { _id: 5b434c6bdc5d9f1ef42d9168, col1: 3, col2: 'aaa333', __v: 0 }
+
+// find all
+AModel.find({})
+// -> [ { _id: 5b434c6bdc5d9f1ef42d9166, col1: 3, col2: 'a3', __v: 0 },
+//      { _id: 5b434c6bdc5d9f1ef42d9167, col1: 3, col2: 'a3', __v: 0 },
+//      { _id: 5b434c6bdc5d9f1ef42d9168, col1: 3, col2: 'aaa333', __v: 0 } ]
+
+// * upsert (special) *
+
+// upsert without condition
+AModel.update({}, { col1: 3, col2: 'AAA333' }, { upsert: true })
+// -> { n: 1, nModified: 1, ok: 1 }
+
+// find all
+AModel.find({})
+// -> [ { _id: 5b434c6bdc5d9f1ef42d9166, col1: 3, col2: 'AAA333', __v: 0 },
+//      { _id: 5b434c6bdc5d9f1ef42d9167, col1: 3, col2: 'a3', __v: 0 },
+//      { _id: 5b434c6bdc5d9f1ef42d9168, col1: 3, col2: 'aaa333', __v: 0 } ]
+```
--- app.js
+++ app.js
@@ -6,11 +6,10 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/db1');
 
 const Schema = mongoose.Schema;
-const Aaa = mongoose.model('Aaa', new Schema({
-  col1: { type: Number },
-  col2: { type: String }
-}));
 
+const AModel = mongoose.model('AModel', new Schema({
+  col1: { type: Number }, col2: { type: String }
+}));
 const o1 = { col1: 3, col2: 'a3' };
 const o2 = { col1: 3, col2: 'aaa333' };
 
@@ -20,23 +19,34 @@ function executable(f) {
 
 Promise.resolve()
   .then(() => console.log('\n// * upsert *'))
-  .then(() => console.log('\n// reset')).then(executable(() => Aaa.remove({})))
-  .then(() => console.log('\n// check content')).then(executable(() => Aaa.findOne({ col1: o1.col1 })))
-  .then(() => console.log('\n// update not found')).then(executable(() => Aaa.update({ col1: o1.col1 }, o1)))
-  .then(() => console.log('\n// insert by upsert')).then(executable(() => Aaa.update({ col1: o1.col1 }, o1, { upsert: true })))
-  .then(() => console.log('\n// check content')).then(executable(() => Aaa.findOne({ col1: o1.col1 })))
-  .then(() => console.log('\n// update')).then(executable(() => Aaa.update({ col1: o1.col1 }, o1)))
-  .then(() => console.log('\n// update the same object')).then(executable(() => Aaa.update({ col1: o1.col1 }, o1, { upsert: true })))
-  .then(() => console.log('\n// update the different object')).then(executable(() => Aaa.update({ col1: o2.col1 }, o2)))
+  .then(() => console.log('\n// reset')).then(executable(() => AModel.remove({})))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
+  .then(() => console.log('\n// update not found')).then(executable(() => AModel.update({ col1: o1.col1 }, o1)))
+  .then(() => console.log('\n// insert by upsert')).then(executable(() => AModel.update({ col1: o1.col1 }, o1, { upsert: true })))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
+  .then(() => console.log('\n// update')).then(executable(() => AModel.update({ col1: o1.col1 }, o1)))
+  .then(() => console.log('\n// update the same object')).then(executable(() => AModel.update({ col1: o1.col1 }, o1, { upsert: true })))
+  .then(() => console.log('\n// update the different object')).then(executable(() => AModel.update({ col1: o2.col1 }, o2)))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
   .then(() => console.log('\n// * save *'))
-  .then(() => console.log('\n// reset')).then(executable(() => Aaa.remove({})))
-  .then(() => console.log('\n// check content')).then(executable(() => Aaa.findOne({ col1: o1.col1 })))
-  .then(() => console.log('\n// save')).then(executable(() => new Aaa(o1).save()))
-  .then(() => console.log('\n// check content')).then(executable(() => Aaa.findOne({ col1: o1.col1 })))
-  .then(() => console.log('\n// save the different object')).then(executable(() => new Aaa(o2).save()))
-  .then(() => console.log('\n// check content')).then(executable(() => Aaa.findOne({ col1: o2.col1 })))
-  .then(() => console.log('\n// find and save')).then(executable(() => Aaa.findOne({ col1: o2.col1 }).then(o => { o.col2 = 'SAVED'; return o.save(); })))
-  .then(() => console.log('\n// check content')).then(executable(() => Aaa.findOne({ col1: o2.col1 })))
+  .then(() => console.log('\n// reset')).then(executable(() => AModel.remove({})))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
+  .then(() => console.log('\n// save')).then(executable(() => new AModel(o1).save()))
+  .then(() => console.log('\n// save the same object')).then(executable(() => new AModel(o1).save()))
+  .then(() => console.log('\n// save the different object')).then(executable(() => new AModel(o2).save()))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
+  .then(() => console.log('\n// findOne and save')).then(executable(() => AModel.findOne({ col1: o2.col1 }).then(o => { o.col2 = 'AAA333'; return o.save(); })))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
+  .then(() => console.log('\n// * create *'))
+  .then(() => console.log('\n// reset')).then(executable(() => AModel.remove({})))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
+  .then(() => console.log('\n// create')).then(executable(() => AModel.create(o1)))
+  .then(() => console.log('\n// create the same object')).then(executable(() => AModel.create(o1)))
+  .then(() => console.log('\n// create the different object')).then(executable(() => AModel.create(o2)))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
+  .then(() => console.log('\n// * upsert (special) *'))
+  .then(() => console.log('\n// upsert without condition')).then(executable(() => AModel.update({}, { col1: 3, col2: 'AAA333' }, { upsert: true })))
+  .then(() => console.log('\n// find all')).then(executable(() => AModel.find({})))
   .then(() => {
     mongoose.disconnect();
   })
@@ -44,74 +54,3 @@ Promise.resolve()
     mongoose.disconnect();
     console.log(e);
   });
-
-// // * upsert *
-//
-// // reset
-// () => Aaa.remove({})
-// { n: 2, ok: 1 }
-//
-// // check content
-// () => Aaa.findOne({ col1: o1.col1 })
-// null
-//
-// // update not found
-// () => Aaa.update({ col1: o1.col1 }, o1)
-// { n: 0, nModified: 0, ok: 1 }
-//
-// // insert by upsert
-// () => Aaa.update({ col1: o1.col1 }, o1, { upsert: true })
-// { n: 1,
-//   nModified: 0,
-//   upserted: [ { index: 0, _id: 5b433b933a9b509714b9982e } ],
-//   ok: 1 }
-//
-// // check content
-// () => Aaa.findOne({ col1: o1.col1 })
-// { _id: 5b433b933a9b509714b9982e, col1: 3, __v: 0, col2: 'a3' }
-//
-// // update
-// () => Aaa.update({ col1: o1.col1 }, o1)
-// { n: 1, nModified: 0, ok: 1 }
-//
-// // update the same object
-// () => Aaa.update({ col1: o1.col1 }, o1, { upsert: true })
-// { n: 1, nModified: 0, ok: 1 }
-//
-// // update the different object
-// () => Aaa.update({ col1: o2.col1 }, o2)
-// { n: 1, nModified: 1, ok: 1 }
-//
-// // * save *
-//
-// // reset
-// () => Aaa.remove({})
-// { n: 1, ok: 1 }
-//
-// // check content
-// () => Aaa.findOne({ col1: o1.col1 })
-// null
-//
-// // save
-// () => new Aaa(o1).save()
-// { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'a3', __v: 0 }
-//
-// // check content
-// () => Aaa.findOne({ col1: o1.col1 })
-// { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'a3', __v: 0 }
-//
-// // save the different object
-// () => new Aaa(o2).save()
-// { _id: 5b433b9333ef93257cc52e79, col1: 3, col2: 'aaa333', __v: 0 }
-//
-// // check content
-// () => Aaa.findOne({ col1: o2.col1 })
-// { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'a3', __v: 0 }
-//
-// // find and save
-// () => Aaa.findOne({ col1: o2.col1 }).then(o => { o.col2 = 'SAVED'; return o.save(); })
-// { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'SAVED', __v: 0 }
-//
-// // check content
-// () => Aaa.findOne({ col1: o2.col1 })
-// { _id: 5b433b9333ef93257cc52e78, col1: 3, col2: 'SAVED', __v: 0 }
